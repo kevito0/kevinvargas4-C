2b77fbfd845987f99581819be75acc2d54a9b0b7
kevito0 <kevinvargas@liceovvh.cl>
2024-04-04 09:03:10 -0300

    ok

---
+++ POO-main/04_condicionales/01_ejemplo/script.js
@@ -0,0 +1,13 @@
+
+Ident() 
+{
+    //identificar si es mayor o menor de edad
+    let edad = prompt("Por favor ingrese edad: ");
+
+    if(edad >= 18){
+        alert(`es mayor de edad: ${edad}`)
+    }
+    else{
+        alert("es menor de edad")
+    }
+}
