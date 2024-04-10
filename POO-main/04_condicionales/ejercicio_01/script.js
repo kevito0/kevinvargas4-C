a78edfe452b76248a7b868820d799b7b174d9ac4
kevito0 <kevinvargas@liceovvh.cl>
2024-04-10 10:12:18 -0400

    ok

--- POO-main/04_condicionales/ejercicio_01/script.js
+++ POO-main/04_condicionales/ejercicio_01/script.js
@@ -3,8 +3,8 @@ function Ident()
 {
     let resultado = "";
 
-    let num1 = prompt("Por favor ingrese numero: ");
-    let num2 = prompt("Por favor ingrese numero: ");
+    let num1 = parseInt(prompt("Por favor ingrese numero: "));
+    let num2 = parseInt(prompt("Por favor ingrese numero: "));
 
     if(num1 > num2){
         resultado = `El mayor es: <h2>${num1}</h2>`
